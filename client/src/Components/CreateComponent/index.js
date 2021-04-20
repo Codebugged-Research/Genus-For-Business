97200edd63376e10e378be880bfea6f94c99adad
Devendra Pratap Singh <ashudev3254@gmail.com>
2021-04-20 18:51:28 +0530

    Dynamic Rendering Of forms

--- client/src/Components/CreateComponent/index.js
+++ client/src/Components/CreateComponent/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { makeStyles} from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import {
     Container
 } from './CreateComponent';
@@ -10,7 +12,7 @@ const useStyles = makeStyles((theme) => ({
         minHeight: '200px',
         minWidth: '350px',
         padding: theme.spacing(2),
-        backgroundColor: 'white'
+        backgroundColor: 'white',
     },
     heading:{
         fontFamily: 'Nunito, sans-serif',
@@ -24,17 +26,53 @@ const useStyles = makeStyles((theme) => ({
         color: 'white',
         width: '100%',
         marginTop: theme.spacing(2)
+    },
+    content: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        marginTop: theme.spacing(2),
+        height: '100%'
+    },
+    contentForm:{
+        display: 'flex'
     }
 }))
 
 function CreateScreen(){
 
     const classes = useStyles();
+    const [formState, setFormState] = useState("");
 
     return(
         <Container>
             <Paper elevation={3} className={classes.paperStyle}>
-
+                <Typography className={classes.heading} variant="h4">User Actions</Typography>
+                <div className={classes.content}>
+                    <Button 
+                        className={classes.btnStyle} 
+                        style={{backgroundColor: '#202950'}} 
+                        variant="contained"
+                        onClick={() => setFormState("create")}
+                    >
+                        Create Meeting
+                    </Button>
+                    <div className={classes.createForm} style={{display: formState === "create" ? 'flex' : 'none'}}>
+                            hwodufwiui
+                    </div>
+                    <Button 
+                        className={classes.btnStyle} 
+                        style={{backgroundColor: '#202950'}} 
+                        variant="contained"
+                        onClick={() => setFormState("join")}
+                    >
+                        Join Meeting
+                    </Button>
+                    <div className={classes.joinForm} style={{display: formState === "join" ? 'flex' : 'none'}}>
+                    uwhefhefuuwiu
+                    </div>
+                </div>
             </Paper>
         </Container>
     )
