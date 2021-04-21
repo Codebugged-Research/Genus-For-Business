ef37971113fee0b65c92c98b06480dfa7a697e07
Devendra Pratap Singh <ashudev3254@gmail.com>
2021-04-21 13:48:11 +0530

    Structuring Interface

---
+++ client/src/Components/Conference/ConferenceComponent.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: grid;
+    width: 100%;
+    height: 100vh;
+    grid-template-columns: 8fr 2fr;
+    background-color: #202960;
+`
+
+export const Holder = styled.div`
+    display: grid;
+    grid-template-rows: 8fr 2fr;
+    grid-gap: 15px;
+    height: 100%;
+`
+
+export const Utils = styled.div`
+    height: 100%;
+    background-color: #2D2B6C;
+    border-radius: 15px;
+`
+export const VideoContainer = styled.div`
+`
+
+export const Actions = styled.div`
+    width: 100%;
+    background-color: #2B2E74;
+`
--- client/src/Components/Conference/index.js
+++ client/src/Components/Conference/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import {
+    Container,
+    Holder,
+    Utils,
+    VideoContainer,
+    Actions
+} from './ConferenceComponent';
 
 function Conference(){
 
     return (
-        <></>
+        <Container>
+            <Holder>
+                <VideoContainer>
+
+                </VideoContainer>
+                <Actions>
+                    
+                </Actions>
+            </Holder>
+            <Utils></Utils>
+        </Container>
     )
 }
 
